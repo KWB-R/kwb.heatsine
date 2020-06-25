1082a05c2dd7407aa60a024a04e7562c7e459a03
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-06-25 17:52:15 +0200

    Clean plot_temperature_interactive()
    Calculate min and max only once using range()
    @mrustl What is as_tibble() required for?

--- R/plot_temperature_interactive.R
+++ R/plot_temperature_interactive.R
@@ -8,21 +8,22 @@
 #' gw_data <- kwb.heatsine::load_temperature_from_csv(path)
 #' kwb.heatsine::plot_temperature_interactive(gw_data)
 #'
-plot_temperature_interactive <- function(df) {
-  df <- tibble::as_tibble(df)
-  g <- ggplot2::ggplot(data = df, ggplot2::aes_string(
-    x = "date",
-    y = "value"
-  )) +
+plot_temperature_interactive <- function(df)
+{
+  value_range <- range(kwb.utils::selectColumns(df, "value"))
+
+  yintercept <- c(
+    value_range[1L], # minimum
+    (value_range[1L] + value_range[2L]) / 2, # mean(c(min, max))
+    value_range[2L] # maximum
+  )
+
+  g <- ggplot2::ggplot(
+    data = tibble::as_tibble(df),
+    ggplot2::aes_string(x = "date", y = "value")
+  ) +
     ggplot2::geom_point() +
-    ggplot2::geom_hline(
-      yintercept = c(
-        min(df$value),
-        (max(df$value) + min(df$value)) / 2,
-        max(df$value)
-      ),
-      lty = 2, colour = "grey"
-    ) +
+    ggplot2::geom_hline(yintercept = yintercept, lty = 2, colour = "grey") +
     ggplot2::scale_x_date(date_labels = "%Y/%m/%d") +
     ggplot2::labs(title = attr(df, "label"), y = "temperature (\u00B0 C)") +
     ggplot2::theme_bw()
